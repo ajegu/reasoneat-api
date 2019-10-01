f96d46b969d5ea43a8619c9907857a710c6f2691
Allan <allanjegu@gmail.com>
2019-10-01 20:02:21 +0200

    init spring boot

---
+++ src/main/java/reasoneatapi/ReasoneatApiApplication.java
@@ -0,0 +1,13 @@
+package reasoneatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReasoneatApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReasoneatApiApplication.class, args);
+	}
+
+}
