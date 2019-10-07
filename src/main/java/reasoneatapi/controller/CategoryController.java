14fe77a59fbfbd32812751069854544b6341dd5a
Allan <allanjegu@gmail.com>
2019-10-07 22:48:30 +0200

    Documentation de l'api avec Swagger

--- src/main/java/reasoneatapi/controller/CategoryController.java
+++ src/main/java/reasoneatapi/controller/CategoryController.java
@@ -1,5 +1,9 @@
 package reasoneatapi.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.SwaggerDefinition;
+import io.swagger.annotations.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -11,12 +15,14 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/categories")
+@Api(tags = {"Catégorie"})
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
     @GetMapping
+    @ApiOperation(value = "Lister les opérations")
     public Page<CategoryDTO> list(
             @RequestParam(required = false, name = "page", defaultValue = "0") int page,
             @RequestParam(required = false, name = "size", defaultValue = "10") int size
@@ -25,21 +31,25 @@ public class CategoryController {
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Afficher une opération")
     public CategoryDTO show(@PathVariable UUID id) {
         return categoryService.findOne(id);
     }
 
     @PostMapping
+    @ApiOperation(value = "Créer une opération")
     public CategoryDTO create(@RequestBody CategoryDTO categoryDTO) {
         return categoryService.save(categoryDTO);
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value = "Modifier une opération")
     public CategoryDTO update(@RequestBody CategoryDTO categoryDTO, @PathVariable UUID id) {
         return categoryService.update(categoryDTO, id);
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Supprimer une opération")
     public void delete(@PathVariable UUID id) {
         categoryService.delete(id);
     }
--- src/main/java/reasoneatapi/dto/CategoryDTO.java
+++ src/main/java/reasoneatapi/dto/CategoryDTO.java
@@ -1,11 +1,15 @@
 package reasoneatapi.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.UUID;
 
 @Data
 public class CategoryDTO {
+    @ApiModelProperty(notes = "L'ID est automatiquement généré par la base de données")
     private UUID id;
+
+    @ApiModelProperty(notes = "Libellé de la catégorie", required = true)
     private String name;
 }
